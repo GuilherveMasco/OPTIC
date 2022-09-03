c0715c58f93a58253fffe992835d618a95e542e0
Guilherme Vasco <guilhermevasco08@gmail.com>
2022-09-03 11:52:08 -0300

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, csv
 
 #IPv4 output data
 path_to_json = 'output-ipv4/'
@@ -9,25 +9,36 @@ avg_sev_ipv4 = 0
 avg_exp_ipv4 = 0
 avg_imp_ipv4 = 0
 
-for item in json_files:
-    item_name = 'output-ipv4/' + item
-    file = open(item_name, 'r')
-    values = json.load(file)
+with open('ipv4_data.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['CVE', 'Severity_score', 'Exploitability_score', 'Impact_score', 'Access_complexity', 'Severity'])
+
+    for item in json_files:
+        item_name = 'output-ipv4/' + item
+        file = open(item_name, 'r')
+        values = json.load(file)
+        
+        #average severity score for IPv4
+        sev_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["baseScore"]
+        avg_sev_ipv4 += sev_score
     
-    #average severity score for IPv4
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["baseScore"]
-    avg_sev_ipv4 += uiReq
+        #average exploitability score for IPv4
+        ex_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["exploitabilityScore"]
+        avg_exp_ipv4 += ex_score
+    
+        #average impact score for IPv4
+        imp_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["impactScore"]
+        avg_imp_ipv4 += imp_score
 
-    #average exploitability score for IPv4
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["exploitabilityScore"]
-    avg_exp_ipv4 += uiReq
+        acc_comp = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["accessComplexity"]
 
-    #average impact score for IPv4
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["impactScore"]
-    avg_imp_ipv4 += uiReq
+        severity = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["severity"]
+    
+        cont += 1
 
-    cont += 1
-
+        writer.writerow([item.strip('.json'), sev_score, ex_score, imp_score, acc_comp, severity])
+    
 #average values for severity, exploitability and impact scores (IPv4)
 avg_sev_ipv4 = avg_sev_ipv4/cont
 avg_exp_ipv4 = avg_exp_ipv4/cont
@@ -43,24 +54,36 @@ avg_sev_ipv6 = 0
 avg_exp_ipv6 = 0
 avg_imp_ipv6 = 0
 
-for item in json_files:
-    item_name = 'output-ipv6/' + item
-    file = open(item_name, 'r')
-    values = json.load(file)
+with open('ipv6_data.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['CVE', 'Severity_score', 'Exploitability_score', 'Impact_score', 'Access_complexity', 'Severity'])
+
+    for item in json_files:
+        item_name = 'output-ipv6/' + item
+        file = open(item_name, 'r')
+        values = json.load(file)
+        
+        #average severity score for IPv6
+        sev_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["baseScore"]
+        avg_sev_ipv6 += sev_score
     
-    #average severity score for IPv6
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["baseScore"]
-    avg_sev_ipv6 += uiReq
+        #average exploitability score for IPv6
+        ex_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["exploitabilityScore"]
+        avg_exp_ipv6 += ex_score
+    
+        #average impact score for IPv6
+        imp_score = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["impactScore"]
+        avg_imp_ipv6 += imp_score
 
-    #average exploitability score for IPv6
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["exploitabilityScore"]
-    avg_exp_ipv6 += uiReq
+        #access complexity
+        acc_comp = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["cvssV2"]["accessComplexity"]
 
-    #average impact score for IPv6
-    uiReq = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["impactScore"]
-    avg_imp_ipv6 += uiReq
+        severity = values["result"]["CVE_Items"][0]["impact"]["baseMetricV2"]["severity"]
 
-    cont += 1
+        cont += 1
+
+        writer.writerow([item.strip('.json'), sev_score, ex_score, imp_score, acc_comp, severity])
 
 #average values for severity, exploitability and impact scores (IPv6)
 avg_sev_ipv6 = avg_sev_ipv6/cont
